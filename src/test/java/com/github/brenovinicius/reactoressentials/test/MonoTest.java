623bf69ae552a13271d2281ee13980c4b3d92b06
Breno Vinicius <24703386+brenovinicius@users.noreply.github.com>
2022-11-19 15:44:11 -0300

    initial structure of the project

---
+++ src/test/java/com/github/brenovinicius/reactoressentials/test/MonoTest.java
@@ -0,0 +1,13 @@
+package com.github.brenovinicius.reactoressentials.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    void test(){
+        log.info("Test Works!");
+    }
+}
